a791fa8f00133e32dd4d9d79b385af04729fee33
brandeecheung <164966810+brandeecheung@users.noreply.github.com>
2024-07-11 20:16:16 -0500

    added api key and token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  trelloApiKey: 'your-trello-api-key',
-  trelloApiToken: 'your-trello-api-token'
+  trelloApiKey: '30ebe61c0617e68ac75a59b557eec22e',
+  trelloApiToken: 'ATTA351eefa34e6aeee88d7ba0f4d63f55ba621d804b220c1caf1496608c33be1349E5D4FE5A'
 };
