cecec0c3fbd801fe1efc6b05ce6508ea8c15c4f4
brandeecheung <164966810+brandeecheung@users.noreply.github.com>
2024-07-11 21:16:50 -0500

    add keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 const keys = require('./config/keys');
-console.log(keys.trelloApiKey); // Logs your Trello API key
-console.log(keys.trelloApiToken); // Logs your Trello API token
+console.log(keys.trelloApiKey); // Logs Trello API key
+console.log(keys.trelloApiToken); // Logs Trello API token
