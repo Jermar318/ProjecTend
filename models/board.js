d561a5d006ca2317f4d3ac854e9365f32dd19728
brandeecheung <164966810+brandeecheung@users.noreply.github.com>
2024-07-15 19:50:49 -0500

    added to models

---
+++ models/board.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Board = sequelize.define('Board', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {});
+
+  Board.associate = (models) => {
+    Board.hasMany(models.List, { foreignKey: 'boardId', onDelete: 'CASCADE' });
+  };
+
+  return Board;
+};
---
+++ models/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+});
+
+const Board = require('./board')(sequelize);
+const List = require('./list')(sequelize);
+const Card = require('./card')(sequelize);
+
+// Define associations
+Board.associate({ List });
+List.associate({ Board, Card });
+Card.associate({ List });
+
+module.exports = { sequelize, Board, List, Card };
