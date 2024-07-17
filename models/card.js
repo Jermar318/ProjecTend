8bc0cd715235f4f2b7756bf2cb6ba3b95e4272d3
brandeecheung <164966810+brandeecheung@users.noreply.github.com>
2024-07-16 23:09:14 -0500

    add models and html routes

--- models/card.js
+++ models/card.js
@@ -1,10 +1,17 @@
 const { Model, DataTypes } = require('sequelize');
+const Status = require('../constants/status');
 
 module.exports = (sequelize) => {
   class Card extends Model {}
 
   Card.init(
     {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       title: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -12,20 +19,32 @@ module.exports = (sequelize) => {
       description: {
         type: DataTypes.TEXT,
       },
-      listId: {
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: Status.TODO
+      },
+      boardId: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        references: {
+          model: 'board',
+          key: 'id',
+        },
       },
+      assignee: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      }
     },
     {
       sequelize,
-      modelName: 'Card',
+      modelName: 'card',
     }
   );
-
-  Card.associate = (models) => {
-    Card.belongsTo(models.List, { foreignKey: 'listId' });
-  };
 
   return Card;
 };
