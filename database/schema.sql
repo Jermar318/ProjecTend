d3236a3fa3fcc1682cab937e5f49132c584f4597
Jermar318 <164591735+Jermar318@users.noreply.github.com>
2024-07-17 21:28:20 -0500

    added tables to schema along with keys

--- database/schema.sql
+++ database/schema.sql
@@ -3,3 +3,32 @@ CREATE DATABASE projectend_db;
 
 \c projectend_db;
 SELECT projectend_db();
+
+CREATE TABLE User (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Board (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+CREATE TABLE Card (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL,
+    board_id INT NOT NULL,
+    assignee_id INT,
+    FOREIGN KEY (user_id) REFERENCES User(id)
+);
