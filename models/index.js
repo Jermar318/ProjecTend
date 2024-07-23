70badd52e24b9bd1e8bc24c182250a90f295d864
brandeecheung <164966810+brandeecheung@users.noreply.github.com>
2024-07-22 20:54:23 -0500

    re ad sequelize for render

--- models/index.js
+++ models/index.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
-  host: process.env.DB_HOST, 
-  port: process.env.DB_PORT, 
-  dialect: 'postgres', 
-  logging: false, 
-});
+let sequelize;
 
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
+    host: process.env.DB_HOST, 
+    port: process.env.DB_PORT, 
+    dialect: 'postgres', 
+    logging: false, 
+    
+  });
+  
+}
 // Import models
 const User = require('./user')(sequelize);
 const Card = require('./card')(sequelize);
